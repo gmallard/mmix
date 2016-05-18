8c8c2aa5167db74aadbfda6b50c36a61b3c24c1b
Guy M. Allard <allard.guy.m@gmail.com>
2016-05-18 02:10:43 -0400

    Generalize helper scripts.

--- bin/osx/mmixasml.sh
+++ bin/osx/mmixasml.sh
@@ -3,5 +3,5 @@
 echo "Compile Program $1.mms"
 echo "Listing is $1.lst"
 echo "Object/executable is $1.mmo"
-./mmixal -l $1.lst $1.mms
+mmixal -l $1.lst $1.mms
 
--- bin/osx/mmixrun.sh
+++ bin/osx/mmixrun.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #
 echo "Run Program $1.mmo"
-./mmix $1.mmo $2
+mmix $1.mmo $2
 
